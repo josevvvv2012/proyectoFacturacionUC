aa8356e2a276da7fec4402d2c1af020b5c98b179
negro <negro@negropc>
2015-09-12 14:22:00 -0500

    Comentario : Se adiciono un imprimir

--- src/proyectofacturacion/ProyectoFacturacion.java
+++ src/proyectofacturacion/ProyectoFacturacion.java
@@ -16,6 +16,7 @@ public class ProyectoFacturacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Esto es una prueba");
     }
     
 }
