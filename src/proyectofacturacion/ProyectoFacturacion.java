82c613fd5a325a248853dddc90d0cfef35fcc86f
Lenovo <Lenovo@Lenovo-PC>
2015-09-12 14:41:11 -0500

    prueba : se adiciono imprimir

--- src/proyectofacturacion/ProyectoFacturacion.java
+++ src/proyectofacturacion/ProyectoFacturacion.java
@@ -17,6 +17,7 @@ public class ProyectoFacturacion {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Esto es una prueba");
+         System.out.println("Esto es una prueba yaneth");
     }
     
 }
