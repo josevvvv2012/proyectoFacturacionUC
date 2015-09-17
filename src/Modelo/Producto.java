fb0eb7dafc8ca92374259766e520426f95ab6700
negro <negro@negropc>
2015-09-16 19:22:42 -0500

    Jose : Se crea los paquetes MVC y clases ,atributos de la clase Producto

---
+++ src/Modelo/Producto.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author negro
+ */
+public class Producto {
+    String NombreProducto;
+    double PrecioVenta;
+    double PrecioCosto;
+    double IvaProducto;
+    double stock;
+
+    public Producto(String NombreProducto, double PrecioVenta, double PrecioCosto, double IvaProducto, double stock) {
+        this.NombreProducto = NombreProducto;
+        this.PrecioVenta = PrecioVenta;
+        this.PrecioCosto = PrecioCosto;
+        this.IvaProducto = IvaProducto;
+        this.stock = stock;
+    }
+
+    public String getNombreProducto() {
+        return NombreProducto;
+    }
+
+    public void setNombreProducto(String NombreProducto) {
+        this.NombreProducto = NombreProducto;
+    }
+
+    public double getPrecioVenta() {
+        return PrecioVenta;
+    }
+
+    public void setPrecioVenta(double PrecioVenta) {
+        this.PrecioVenta = PrecioVenta;
+    }
+
+    public double getPrecioCosto() {
+        return PrecioCosto;
+    }
+
+    public void setPrecioCosto(double PrecioCosto) {
+        this.PrecioCosto = PrecioCosto;
+    }
+
+    public double getIvaProducto() {
+        return IvaProducto;
+    }
+
+    public void setIvaProducto(double IvaProducto) {
+        this.IvaProducto = IvaProducto;
+    }
+
+    public double getStock() {
+        return stock;
+    }
+
+    public void setStock(double stock) {
+        this.stock = stock;
+    }
+       
+}
